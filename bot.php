071152b7956102c589f128119d071c6402187d00
genbtcdoubler <79389495+genbtcdoubler@users.noreply.github.com>
2021-03-06 10:41:27 -0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -29,18 +29,19 @@ $b|  HACKING - TOOLS : [31m GHANA$b        |
 
 while (true){
     
-$url="xxxx";
+$url="https://bitcoinsp.in/ajax/getCoin";
 
-$req="xxxx";
+$req="sd=20&google-response-token=&g-recaptcha-response=03AGdBq27NyI_1ocIzS2TZrPfE4QIQUzXNdp9rhW6JVQXW-zJFxdqQ0_neyK3rXv2Hfb4ptg8fZc75fc3wGQpoM9oc4HTqaNKS9-2h2420Ysz2Ep816qOB4SCi9nvMWON-D8sv4PRwoaZz8OL93yf17if_X1DQbOx75F3CrxwdmTpiQiUtsrCKpQKQYi0e61eNtEygf_pPDr2eCsRWAx5T1bzZGjHuU00WlKO-Wktck_YRbGGovxxTiH-5EaXxEiyQyJTh_nWzgJM-QuN5vENtkyykwh3TDFod7Sdpp8Lcyckb27Y3h68-ChW34N5OjPU878dgWyQyS1enzcLIw2PnaDxZqLm2p2xBS0xQYkDY-nYV32s4S39gtNh98Rr47fBcIL4AedqTDkdVgXp04ivFbIbukRZBmvyyA6RujxURx39EWCNIJuusdbkMtB8OZIRkrwIuVSxdb8O6raKvSGMl-9-yrB5XWUtnhQ&captcha-letra=
+";
 
 $hacker=curl_init();
 curl_setopt_array($hacker, array(
-	CURLOPT_PORT =>"xxxx",
+	CURLOPT_PORT =>"443",
 CURLOPT_URL => "$url",
 	CURLOPT_RETURNTRANSFER => true,
 	CURLOPT_SSL_VERIFYPEER => false,
 	CURLOPT_TIMEOUT => 30,
-	CURLOPT_CUSTOMREQUEST => "xxxx",
+	CURLOPT_CUSTOMREQUEST => "POST",
 	CURLOPT_POSTFIELDS => $req,
 ));
 	
@@ -48,7 +49,7 @@ CURLOPT_URL => "$url",
 $obenz = curl_exec($hacker);
 curl_close($hacker);
 $js = json_decode($obenz,true);
-echo "xxxx : ".$js["xxxx"]."xxxx".$js["xxxx"]."\n";
+echo "alert alert-success : ".$js["alert alert-success"]."#balance-center".$js["#balance-center"]."\n";
 sleep(5);
 }
 ?>
